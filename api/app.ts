3345c2022e947c750c18f3651bf918c8396634a7
Pablo <pablosalgadou@gmail.com>
2023-03-09 16:28:51 -0300

    add documentation

--- api/app.ts
+++ api/app.ts
@@ -4,10 +4,13 @@ import regionsRouter from './v1/routes/regions.routes';
 import region from './database/regions';
 import provincesRouter from './v1/routes/provinces.routes';
 import communesRouter from './v1/routes/communes.routes';
-dotenv.config()
+import swaggerUI from 'swagger-ui-express'
+import swaggerDoc from '../documentation.json'
+if (process.env.NODE_ENV === "development") {
+    dotenv.config()
+}
 
-const app = express();
-
+export const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
@@ -20,10 +23,11 @@ app.use('/api/v1/add-country', async (req, res, next) => {
 app.use('/api/v1/provinces', provincesRouter);
 app.use('/api/v1/regions', regionsRouter);
 app.use('/api/v1/communes', communesRouter);
+app.use('/', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 
 
-app.listen(process.env.PORT, () => console.log(`Aplicacion corriendo en el puerto ${process.env.PORT}`));
-// TOPOJSON ?
+export const server = app.listen(process.env.PORT, () => console.log(`Aplicacion corriendo en el puerto ${process.env.PORT}`));
+
 
 
 
